81017496a0d972c713d20a46f188f7f99f8169ea
Iván Rodríguez Conde <ivanrodriguezconde@outlook.com>
2017-07-08 22:24:06 +0200

    Script refactored and finished

---
+++ infojobs_handler.py
@@ -0,0 +1,89 @@
+
+
+import requests
+import json
+import datetime
+
+class InfojobsHandler:
+
+    def __init__(self, config_file):
+
+        self.url = 'https://api.infojobs.net/api/1/offer'
+        self.auth_data = None
+        self.parameters = None
+        self.config_file = config_file
+        self.offer_list = None
+
+    def load_config(self):
+
+        json_string = open(self.config_file).read()
+        json_data = json.loads(json_string)
+        self.auth_data = (json_data['auth_data']['client_id'], json_data['auth_data']['client_secret'])
+        self.parameters = self.process_query_parameters(json_data['parameters'])
+
+
+    def process_query_parameters(self, json_data):
+
+        # provinces parameters
+
+        aux_parameters = ()
+        for province in json_data['provinces']:
+            current_prov = ('province', province)
+            aux_parameters = (current_prov,) + aux_parameters
+
+        # keywords parameters
+
+        aux_keyword = "("
+        for keyword in json_data['keywords']:
+            aux_keyword += keyword + " "
+        aux_keyword += ")"
+
+        keywords_tuple = ('q', aux_keyword)
+        aux_parameters = (keywords_tuple,) + aux_parameters
+
+        # publish date
+
+        time_window = int(json_data['time-window'])
+
+        date = datetime.datetime.utcnow() - datetime.timedelta(minutes=time_window)
+        date = date.replace(microsecond=0)
+        date = date.isoformat('T') + 'Z'
+
+        date_tuple = ('publishedMin', date)
+        aux_parameters = (date_tuple,) + aux_parameters
+
+        return aux_parameters
+
+    def query_job_offers(self):
+
+        if self.auth_data is not None and self.parameters is not None:
+
+            response = requests.get(self.url, auth=self.auth_data, params=self.parameters)
+            response_dic = json.loads(response.content)
+            self.offer_list = response_dic['offers']
+        else:
+            self.offer_list = None
+
+    def get_offers_summary(self):
+
+        summary = u""
+
+        for offer in self.offer_list:
+
+            title = offer['title']
+
+            summary += title
+            summary += "\n"
+            summary += offer['link']
+            summary += "\n"
+            summary = summary + "Salario: [" + offer['salaryMin']['value'] + " - " + offer['salaryMax']['value'] + "]"
+            summary += "\n"
+            summary = summary + "Publicacion: " + offer['published']
+            summary += "\n"
+            summary += "\n"
+            summary += "-----------------------------------------------------------------------------------------------"
+            summary += "\n"
+            summary += "\n"
+
+        return summary.encode('utf-8')
+
--- main.py
+++ main.py
@@ -1,37 +1,44 @@
-import requests
-
-# API URIs
-
-base_url = "https://api.infojobs.net/api/1"
-get_job_offers = "/offer"
-get_provinces = "/dictionary/province"
-get_categories = "/dictionary/category"
-get_subcategories = "/dictionary/subcategory"
-get_cities= "/dictionary/city"
+#!/usr/bin/python
 
 
-# Authentication
-
-client_id = "ab7ac04688174782a8269c5adc7bde6c"
-client_secret = "Gnt7Tqgb4fUctqOMKXWjJg9YPGKKPP+PDgd6r/B0tdsjPEqbmp"
-
-# Parameters
-
-keywords = ''
-province = 'Ourense'
-city = 'Ourense'
-payload = {'q': keywords, 'province': province}
-
-#publishedMax
+from infojobs_handler import InfojobsHandler
+import smtplib
+from email.mime.text import MIMEText
+import sys
 
 
+def main():
+
+    if len(sys.argv) != 5:
+        print "Incorrect number of input parameters"
+        return
+
+    from_mail = sys.argv[1]
+    from_pass = sys.argv[2]
+    to_mail = sys.argv[3]
+    config_file = sys.argv[4]
+
+    # We get a json from server with job offers
+
+    infojobs_manager = InfojobsHandler(config_file)
+    infojobs_manager.load_config()
+    infojobs_manager.query_job_offers()
+
+    if infojobs_manager.offer_list is not None and infojobs_manager.offer_list != []:
+
+        msg = MIMEText(str(infojobs_manager.get_offers_summary()))
+        msg['Subject'] = 'Nuevas ofertas de InfoJobs'
+        msg['From'] = from_mail
+        msg['To'] = to_mail
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(from_mail, from_pass)
+        server.sendmail(from_mail, [to_mail], msg.as_string())
+        server.quit()
 
 
-# Main query
-
-url = base_url + get_job_offers
-myResponse = requests.get(url, auth=(client_id, client_secret), params=payload)
-
-
-print (myResponse.content)
+if __name__ == "__main__":
+    main()
 
